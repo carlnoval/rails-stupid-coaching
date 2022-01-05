732e444e743b0866f53f245fee1076c8aaae9179
Carl Noval <cjrnoval22@gmail.com>
2022-01-05 13:26:22 +0900

    finalized scss, i am loving scss

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,14 +7,14 @@ class QuestionsController < ApplicationController
     # If the message is I am going to work, the coach will answer Great!
     # If the message has a question mark ? at the end, the coach will answer Silly question, get dressed and go to work!.
     # Otherwise the coach will answer I don't care, get dressed and go to work!
-    # raise
-    question = params[:question]
+    question = params[:question].strip
 
     if question.downcase == "i am going to work"
       @coach_reply = "Great!"
     elsif question[-1] == "?"
       @coach_reply = "Silly question, get dressed and go to work!"
     else
+      params[:question] = "-no message-" if question.empty?
       @coach_reply = "I don't care, get dressed and go to work!"
     end
   end
